2b413053ae54a36be9df078ec89ae5762f085c0e
Nikky Kumar <knikki7505@gmail.com>
2024-10-16 04:50:52 +0000

    new one

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,11 @@ const accountId = 144453
 let accountEmail = "nikky@gmail.com"
 var acc_pass = "123"
 accounCity = "Jaipur";
+let account;
 
-accountId = 10;
-console.log(accountId);
+// accountId = 10;
+accountEmail = "knikky@gmail.com"
+acc_pass = "4444"           
+accounCity = "Bangloru"
+console.table([ accountEmail,acc_pass, accounCity, account])
+
