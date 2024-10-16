66844f60dc9763a61771ec7e09b81ea0db447daa
Nikky Kumar <knikki7505@gmail.com>
2024-10-16 06:44:55 +0000

    datatypes

---
+++ basics/03Conversion.js
@@ -0,0 +1,18 @@
+"use strict"
+ let score = "nikky"
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+ 
+
+ let loggedIn = ""
+
+let boolLoggedIn = Boolean(loggedIn)
+// console.log(boolLoggedIn);
+
+let num = 33
+let str = String(num)
+console.log(typeof str);
